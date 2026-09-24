51bebae3917a99c69c8db0b5c7f64cfc6e909a17
Vladimir Fedorenko <fffvladimir952@gmail.com>
2026-09-25 02:06:24 +0300

    Update CORS settings to include production URL
    Added support for production environment with Render.

--- server.js
+++ server.js
@@ -18,10 +18,15 @@ app.get("/", (req, res) => {
 });
 
 app.use(cors({
-  origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+  origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://domshop.onrender.com"
+  ],
   methods: ["GET", "POST", "DELETE"],
   allowedHeaders: ["Content-Type"]
 }));
+
 
 app.use(express.json());
 
